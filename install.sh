bdf2a223bc967b77d6a94ca35196f454cfd3b6de
pwnpope <108923813+pwnpope@users.noreply.github.com>
2023-06-24 23:28:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install git vim curl exuberant-ctags npm nodejs
+sudo apt install git vim curl exuberant-ctags npm nodejs python3-pip
+pip3 install jedi
 
 # install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
