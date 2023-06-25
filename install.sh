cfae7cafb3f9637726467d910769bdfb79f32d33
pwnpope <108923813+pwnpope@users.noreply.github.com>
2023-06-24 23:23:43 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+sudo apt update
+sudo apt install git vim curl exuberant-ctags npm nodejs
+
+# install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+
+
+# vim plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# yarn install and build 
+cwd=$(pwd)
+cd $(sudo find /* | grep 'plugged/coc.nvim' -m 1)
+sudo npm install -g yarn
+yarn install
+yarn build
+
+# navigate back to OG directory
+cd $cwd
+echo "\n[+]finished installing!"
+
+# create config dir if it doesn't already exist
+# create nvim dir if it doesn't already exist
+# write config contents to init.vim for nvim config
+mkdir ~/.config;mkdir ~/.config/nvim
+cat config > ~/.config/nvim/init.vim
