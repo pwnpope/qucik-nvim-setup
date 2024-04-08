3182c4a1a97f50df3e686b7c3b038ee6a05a6e79
pwnpope <108923813+pwnpope@users.noreply.github.com>
2024-04-08 16:06:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,23 @@ pip3 install jedi pylint
 # install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
-
 
 # vim plugin manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+
+curl -O https://nodejs.org/dist/v20.12.1/node-v20.12.1-linux-x64.tar.xz node-v20.12.1-linux-x64.tar.xz
+tar -xvf node-v20.12.1-linux-x64.tar.xz
+sudo apt remove nodejs
+sudo ln -sf /home/pope/Downloads/node-v20.12.1-linux-x64/bin/node /usr/bin/node
+
+
+npm update
 cwd=$(pwd)
 cd ~/.local/share/nvim/plugged/coc.nvim
 npm install
+npm ci
 
 # create config dir if it doesn't already exist
 # create nvim dir if it doesn't already exist
